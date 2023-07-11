e966ee022acb92e95371a52f7e7e44bc39c3d71d
Jonathan Lake <jonlake5@gmail.com>
2023-07-11 17:30:24 -0400

    Added functionality to compare if subscription has correct arn

--- logSubscriptionValidation.py
+++ logSubscriptionValidation.py
@@ -7,7 +7,7 @@ import sys
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Validate Log Subscriptions for an Account')
     parser.add_argument('-p','--profile_name',required=False, type=str)
-    parser.add_argument('-f','--expectedDestination',required=False, type=str)
+    parser.add_argument('-f','--expected_destination',required=False, type=str)
     args = parser.parse_args()
 
     
@@ -30,8 +30,35 @@ you do not need to specify a profile on the command line.\
     client = boto3.client('logs')
     response = client.describe_log_groups()
     logGroups = response['logGroups']
-    if args.expectedDestination:
-        pass
+    if args.expected_destination:
+        output = []
+        filters = []
+        for logGroup in logGroups:
+            # print(logGroup['logGroupName'])
+            paginator = client.get_paginator('describe_subscription_filters')
+            filter_matches = False
+            for response in paginator.paginate(logGroupName=logGroup['logGroupName']):
+                #while filter_matches == False:          
+                for filter in response['subscriptionFilters']:
+                    if filter['destinationArn'] == args.expected_destination:
+                        filter_matches = True
+            if filter_matches == False:
+                print("%s did not have the correct destinationArn" % logGroup['logGroupName'])
+            else:
+                print('%s had the correct destinationArn' % logGroup['logGroupName'])
+                            #print('%s, %s, %s' % (filter['logGroupName'],filter['filterName'],filter['destinationArn']))
+                        # filters.append(
+                        #     {
+                        #         'logGroupName': filter['logGroupName'],
+                        #         'filterName': filter['filterName'],
+                        #         'filterDestination': filter['destinationArn']
+                        #     }
+                        # )
+                # print('%s %s %s' % (response['filterName'], response['logGroupName'], response['destinationArn']))
+                # filterName = response['subscriptionFilters']['filterName']
+        # for filter in filters:
+        #     print(filter)
+                    
     else:
         
         output = []
